44b53d8c9a42f738ab8890fc288ac047b2c37829
lerrorgk <lerrorgk@163.com>
2024-02-24 14:50:53 +0800

    fix 34

--- 34. 在排序数组中查找元素的第一个和最后一个位置/main.cpp
+++ 34. 在排序数组中查找元素的第一个和最后一个位置/main.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        if(nums[nums.size() - 1] < target){
-            return nums.size();
-        }
-        if(nums[0] > target){
-            return 0;
-        }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {left_bound(nums, target), right_bound(nums, target)};
+    }
+
+private:
+    int left_bound(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-        while(left + 1 < right){
-            int middle = left + (right - left) / 2;
-            if(nums[middle] == target){
-                return middle;
-            }
-            if(nums[middle] < target){
-                left = middle;
-            }
-            if(nums[middle] > target){
-                right = middle;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid - 1;
             }
         }
-        if(nums[left] == target){
-            return left;
+        if (left < 0 || left >= nums.size()) {
+            return -1;
         }
-        if(nums[right] == target){
-            return right;
+        return nums[left] == target ? left : -1;
+    }
+
+
+    int right_bound(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                left = mid + 1;
+            }
         }
-        if(left + 1 > nums.size() - 1 || nums[left + 1] > target){
-            return left + 1;
+        if (left - 1 < 0 || left - 1 >= nums.size()) {
+            return -1;
         }
-        return -1;
+        return nums[left - 1] == target ? (left - 1) : -1;
     }
 };
