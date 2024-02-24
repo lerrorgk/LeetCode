c351107a95ba6e86ab39b0111fbb85e866651070
lerrorgk <lerrorgk@163.com>
2024-02-24 14:31:02 +0800

    fix 35

--- 35. 搜索插入位置/main.cpp
+++ 35. 搜索插入位置/main.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if(nums[nums.size() - 1] < target){
-            return nums.size();
-        }
-        if(nums[0] > target){
-            return 0;
-        }
         int left = 0;
         int right = nums.size() - 1;
-        while(left + 1 < right){
+        while(left <= right){
             int middle = left + (right - left) / 2;
             if(nums[middle] == target){
                 return middle;
-            }
-            if(nums[middle] < target){
-                left = middle;
-            }
-            if(nums[middle] > target){
-                right = middle;
+            } else if(nums[middle] < target){
+                left = middle + 1;
+            } else if(nums[middle] > target){
+                right = middle - 1;
             }
         }
-        if(nums[left] == target){
-            return left;
-        }
-        if(nums[right] == target){
-            return right;
-        }
-        if(left + 1 > nums.size() - 1 || nums[left + 1] > target){
-            return left + 1;
-        }
-        return -1;
+        return right + 1;
     }
 };
