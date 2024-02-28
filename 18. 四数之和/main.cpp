7298a59fab7c1d6bc172595a0f141239e08c9758
lerrorgk <lerrorgk@163.com>
2024-02-28 15:29:44 +0800

    18

---
+++ 18. 四数之和/main.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++){
+            if (nums[i] > target && nums[i] >= 0) {
+            	break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for(int j = i + 1; j < nums.size(); j++){
+                if (nums[i] + nums[j] > target && nums[i] + nums[j] >= 0) {
+                    break;
+                }
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.size() - 1;
+                while(left < right){
+                    if ((int64_t) nums[i] + nums[j] + nums[left] + nums[right] > target) {
+                        right--;
+                    } else if ((int64_t) nums[i] + nums[j] + nums[left] + nums[right]  < target) {
+                        left++;
+                    } else {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (right > left && nums[left] == nums[left + 1]) left++;
+                        left++, right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
