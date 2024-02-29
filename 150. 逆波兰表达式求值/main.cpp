0b6ad379bdd8dbb5b87f3606999bda8de7e158f2
lerrorgk <lerrorgk@163.com>
2024-02-29 18:22:25 +0800

    150

---
+++ 150. 逆波兰表达式求值/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<long long> st;
+        for(string t : tokens){
+            if(t == "+" || t == "-" || t == "*" || t == "/"){
+                long long b = st.top();
+                st.pop();
+                long long a = st.top();
+                st.pop();
+                if(t == "+") st.push(a + b);
+                else if(t == "-") st.push(a - b);
+                else if(t == "*") st.push(a * b);
+                else if(t == "/") st.push(a / b);
+            } else{
+                st.push(stoll(t));
+            }
+        }
+        return st.top();
+    }
+};
