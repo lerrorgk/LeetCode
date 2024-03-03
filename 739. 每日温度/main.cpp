7e96d1e996173ce30910bce34330fb493f930f05
lerrorgk <lerrorgk@163.com>
2024-03-03 14:57:48 +0800

    739

---
+++ 739. 每日温度/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        stack<int> st;
+        vector<int> ans(T.size(), 0);
+        for(int i = 0; i < T.size(); i++){
+            if(st.empty() || T[i] <= T[st.top()]){
+                st.push(i);
+            } else{
+                while(!st.empty() && T[i] > T[st.top()]){
+                    ans[st.top()] = i - st.top();
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return ans;
+    }
+};
